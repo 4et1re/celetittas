ecf31be6378d656e072fe770536e1ed11a7eaaa0
4et1re <4et1ree@gmail.com>
2022-07-31 02:24:35 +0300

    Procedural generation
    infinity trail

---
+++ Celeritas/Assets/Scripts/ChunkPlacer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkPlacer : MonoBehaviour
+{
+    
+    
+    public Transform Player;
+    public Chunk[] ChunkPrefabs;
+    public Chunk FirstChunk;
+
+    private List<Chunk> spawnedChunks = new List<Chunk>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnedChunks.Add(FirstChunk);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+        if(Player.position.x > spawnedChunks[spawnedChunks.Count - 1].End.position.x - 10)
+        {
+            SpawnChunk();
+        }
+    }
+
+    private void SpawnChunk()
+    {
+        Chunk newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)]);
+        newChunk.transform.position = spawnedChunks[spawnedChunks.Count - 1].End.position - newChunk.Begin.localPosition;
+        spawnedChunks.Add(newChunk);
+
+        if(spawnedChunks.Count > 10)
+        {
+            spawnedChunks.RemoveAt(0);
+            Destroy(spawnedChunks[0].gameObject);
+            Destroy(FirstChunk);
+        }
+    }
+}
---
+++ Celeritas/Assets/Scripts/testGeneration.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testGeneration : MonoBehaviour
+{
+
+    public int NumberOfBiome = 4;
+    private int id;
+    public int NumberOfPrefab = 10;
+    private int ChoosedPrefab;
+    private int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            id = Random.Range(1, NumberOfBiome);
+        }
+
+        if (id == 1) //winter
+        {
+            while (count < 10)
+            {
+                count++;
+                ChoosedPrefab = Random.Range(1, NumberOfPrefab);
+                Debug.Log("winter" + ChoosedPrefab);
+
+            }
+
+        }
+
+        if (id == 2) //spring
+        {
+            while (count < 10)
+            {
+                count++;
+                ChoosedPrefab = Random.Range(1, NumberOfPrefab);
+                Debug.Log("spring" + ChoosedPrefab);
+
+            }
+
+        }
+
+        if (id == 3) //summer
+        {
+            while (count < 10)
+            {
+                count++;
+                ChoosedPrefab = Random.Range(1, NumberOfPrefab);
+                Debug.Log("summer" + ChoosedPrefab);
+
+            }
+
+        }
+        if (id == 4) //autumn
+        {
+            while (count < 10)
+            {
+                count++;
+                ChoosedPrefab = Random.Range(1, NumberOfPrefab);
+                Debug.Log("autumn" + ChoosedPrefab);
+
+            }
+
+        }
+
+
+
+    }
+}
